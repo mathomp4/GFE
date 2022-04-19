b78d8431f4e1cc7ac1b9d0c67085fc5d9cc85892
Tom Clune <thomas.l.clune@nasa.gov>
2022-04-19 09:19:56 -0400

    Updated pFUnit submodule.

--- ChangeLog.cmd
+++ ChangeLog.cmd
@@ -5,6 +5,22 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.2] - 2022-04-19
+
+### Changed
+
+- Updated pFUnit submodule to include a bugfix.
+
+## [1.0.1] - 2022-04-11
+
+### Changed
+
+- renamed default branch to `main`
+
+### Fixed
+
+- Typos in README
+
 ## [1.0.0] - 2022-04-08
 
 Initial release
